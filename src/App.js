1532e16a467378cf185fb899ca23ce7bd8588bf7
Abhimanyu Kumar <abhimanyukumar82982@gmail.com>
2022-12-31 18:34:07 +0530

    temp added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addData, deleteData } from './actions';
+const App = () => {
 
-class App extends Component {
-  render() {
+   const todo = useSelector((state) => state.data.todo);
+   const dispatch = useDispatch();
     return (
       <div className="App">
         <h1>Hello World!</h1>
+        <button onClick={()=>dispatch(addData())}>ADD DATA</button>
+        <button onClick={()=>dispatch(deleteData())}>DELETE DATA</button>
+        {
+          todo && <div>
+            {JSON.stringify(todo)}
+          </div>
+        }
       </div>
     );
-  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  <Provider store={store}>
   <App />,
+  </Provider>
 );
